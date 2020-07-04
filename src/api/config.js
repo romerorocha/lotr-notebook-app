8271c4200b0a342bc9d4ffdda53e9337f3d48aff
Claudio Romero <romerolrocha@gmail.com>
2020-07-04 14:06:34 -0300

    Extract API URL to .env

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 1000,
   headers: { Authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}` },
 });
