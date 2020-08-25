1822d173484115a3fdd4c2023ec64e2cc30cab89
Claudio Romero <romerolrocha@gmail.com>
2020-08-24 21:25:09 -0300

    Format book component layout

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -5,7 +5,9 @@ import Avatar from '@material-ui/core/Avatar';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Star from '@material-ui/icons/Star';
+import { Grid } from '@material-ui/core';
 
 const getReviewsAverage = book => {
   if (!book.reviews || book.reviews.length === 0) {
@@ -28,9 +30,24 @@ const Book = ({ book }) => {
         title={book.name}
       />
       <CardContent>
-        <Typography variant="body2">
-          Aqui vai a review escolhida para aparecer
-        </Typography>
+        <Grid container spacing={1} alignItems="center">
+          <Grid item>
+            <Typography variant="subtitle2">Pedro Júnior</Typography>
+          </Grid>
+          <Grid item xs>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body2">
+            Esse é um dos melhores livros da minha vida. Nunca pensaei que eu
+            pudesse consumir uma obra dessa magnitude!
+          </Typography>
+        </Grid>
       </CardContent>
       <CardActions>
         <Button>Details</Button>
