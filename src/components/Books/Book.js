240ffadb65c01ae8f95f74943f985aa208d27573
Claudio Romero <romerolrocha@gmail.com>
2020-08-24 21:36:58 -0300

    Improve stars viewing

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -7,7 +7,15 @@ import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Star from '@material-ui/icons/Star';
+import StarBorder from '@material-ui/icons/StarBorder';
 import { Grid } from '@material-ui/core';
+
+const review = {
+  author: 'Pedro Júnior',
+  stars: 3,
+  text:
+    'Esse é um dos melhores livros da minha vida. Nunca pensaei que eu pudesse consumir uma obra dessa magnitude!',
+};
 
 const getReviewsAverage = book => {
   if (!book.reviews || book.reviews.length === 0) {
@@ -22,6 +30,18 @@ const getReviewsAverage = book => {
   return soma / book.reviews.length;
 };
 
+const Stars = ({ number }) => {
+  const stars = [];
+  for (let i = 0; i < number; i++) {
+    stars.push(<Star key={i} />);
+  }
+
+  while (stars.length < 5) {
+    stars.push(<StarBorder />);
+  }
+  return stars;
+};
+
 const Book = ({ book }) => {
   return (
     <Card>
@@ -32,21 +52,14 @@ const Book = ({ book }) => {
       <CardContent>
         <Grid container spacing={1} alignItems="center">
           <Grid item>
-            <Typography variant="subtitle2">Pedro Júnior</Typography>
+            <Typography variant="subtitle2">{review.author}</Typography>
           </Grid>
           <Grid item xs>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Stars number={review.stars} />
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="body2">
-            Esse é um dos melhores livros da minha vida. Nunca pensaei que eu
-            pudesse consumir uma obra dessa magnitude!
-          </Typography>
+          <Typography variant="body2">{review.text}</Typography>
         </Grid>
       </CardContent>
       <CardActions>
